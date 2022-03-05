bb7478b8b976d10d78302655c1169d57cb3dd76e
CarlosMatallana-prog <3496@holbertonschool.com>
2022-03-04 19:51:33 -0500

    :sparkles: Add amenity, city, place, review, and state models

--- console.py
+++ console.py
@@ -6,12 +6,11 @@ import json
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-
-# from models.amenity import Amenity
-# from models.city import City
-# from models.place import Place
-# from models.review import Review
-# from models.state import State
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Module for storing and reload from JSON file"""
-# from models.amenity import Amenity
-# from models.review import Review
-# from models.place import Place
-# from models.state import State
-# from models.city import City
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 import json
 
 
